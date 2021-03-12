916491106a4c83013fb3427917bf6a373419a7d8
SoujanyaJammu <SoujanyaJammu@gmail.com>
2021-03-12 13:20:41 +0530

    cucumber assignment for Orange HRM Application

---
+++ test-output/report.js
@@ -0,0 +1,477 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/mandulas/eclipse-workspace/OrangeHRPApplication/src/test/java/com/qa/features/orange.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Validating the OrangeHRP Application",
+  "description": "",
+  "id": "validating-the-orangehrp-application",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 283200,
+  "status": "passed"
+});
+formatter.background({
+  "line": 2,
+  "name": "Initialize the browser",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 3,
+  "name": "the user opens the browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 4,
+  "name": "the user runs the URL",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Orange_StepDefination.the_user_opens_the_browser()"
+});
+formatter.result({
+  "duration": 7801022100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.the_user_runs_the_URL()"
+});
+formatter.result({
+  "duration": 8506238900,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 7,
+  "name": "Login to OrangeHRP Application",
+  "description": "",
+  "id": "validating-the-orangehrp-application;login-to-orangehrp-application",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 6,
+      "name": "@SmokeTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 8,
+  "name": "I am on OrangeHRP Application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 9,
+  "name": "Login to Application",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "Dashboard page is avaliable",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "Click on Admin Menu",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "Close the Browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Orange_StepDefination.i_am_on_OrangeHRP_Application()"
+});
+formatter.result({
+  "duration": 35580100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.login_to_Application()"
+});
+formatter.result({
+  "duration": 4722393600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.dashboard_page_is_avaliable()"
+});
+formatter.result({
+  "duration": 46483800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.click_on_Admin_Menu()"
+});
+formatter.result({
+  "duration": 768717600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.close_the_Browser()"
+});
+formatter.result({
+  "duration": 1445142700,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 89500,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 123100,
+  "status": "passed"
+});
+formatter.background({
+  "line": 2,
+  "name": "Initialize the browser",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 3,
+  "name": "the user opens the browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 4,
+  "name": "the user runs the URL",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Orange_StepDefination.the_user_opens_the_browser()"
+});
+formatter.result({
+  "duration": 5077523800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.the_user_runs_the_URL()"
+});
+formatter.result({
+  "duration": 7863747000,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 15,
+  "name": "Validating Admin Menu",
+  "description": "",
+  "id": "validating-the-orangehrp-application;validating-admin-menu",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 14,
+      "name": "@SmokeTest"
+    },
+    {
+      "line": 14,
+      "name": "@RegressionalTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 16,
+  "name": "I am on OrangeHRP Application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 17,
+  "name": "Login to Application",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 18,
+  "name": "Click on Admin Menu",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "Click on Job",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 20,
+  "name": "validate text on Job Title",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 21,
+  "name": "Close the Browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Orange_StepDefination.i_am_on_OrangeHRP_Application()"
+});
+formatter.result({
+  "duration": 29041500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.login_to_Application()"
+});
+formatter.result({
+  "duration": 3981682300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.click_on_Admin_Menu()"
+});
+formatter.result({
+  "duration": 763685800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.click_on_Job()"
+});
+formatter.result({
+  "duration": 1385046300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.validate_text_on_Job_Title()"
+});
+formatter.result({
+  "duration": 36091900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.close_the_Browser()"
+});
+formatter.result({
+  "duration": 1269619000,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 82400,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 60600,
+  "status": "passed"
+});
+formatter.background({
+  "line": 2,
+  "name": "Initialize the browser",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 3,
+  "name": "the user opens the browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 4,
+  "name": "the user runs the URL",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Orange_StepDefination.the_user_opens_the_browser()"
+});
+formatter.result({
+  "duration": 4974733700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.the_user_runs_the_URL()"
+});
+formatter.result({
+  "duration": 7473598600,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 23,
+  "name": "Validating PIM Menu",
+  "description": "",
+  "id": "validating-the-orangehrp-application;validating-pim-menu",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 22,
+      "name": "@RegressionalTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 24,
+  "name": "I am on OrangeHRP Application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 25,
+  "name": "Login to Application",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 26,
+  "name": "Click on PIM menu",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 27,
+  "name": "validate user on PIM Menu",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 28,
+  "name": "Close the Browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Orange_StepDefination.i_am_on_OrangeHRP_Application()"
+});
+formatter.result({
+  "duration": 29356600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.login_to_Application()"
+});
+formatter.result({
+  "duration": 3830678800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.click_on_PIM_menu()"
+});
+formatter.result({
+  "duration": 750087900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.validate_user_on_PIM_Menu()"
+});
+formatter.result({
+  "duration": 18998900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.close_the_Browser()"
+});
+formatter.result({
+  "duration": 1339517500,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 62000,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 118800,
+  "status": "passed"
+});
+formatter.background({
+  "line": 2,
+  "name": "Initialize the browser",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 3,
+  "name": "the user opens the browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 4,
+  "name": "the user runs the URL",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Orange_StepDefination.the_user_opens_the_browser()"
+});
+formatter.result({
+  "duration": 4481967700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.the_user_runs_the_URL()"
+});
+formatter.result({
+  "duration": 7046508700,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 30,
+  "name": "Validating Time Menu",
+  "description": "",
+  "id": "validating-the-orangehrp-application;validating-time-menu",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 29,
+      "name": "@SmokeTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 31,
+  "name": "I am on OrangeHRP Application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 32,
+  "name": "Login to Application",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 33,
+  "name": "Click on Time menu",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 34,
+  "name": "validate user on Time Menu",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 35,
+  "name": "Close the Browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Orange_StepDefination.i_am_on_OrangeHRP_Application()"
+});
+formatter.result({
+  "duration": 17988500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.login_to_Application()"
+});
+formatter.result({
+  "duration": 3856921300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.click_on_Time_menu()"
+});
+formatter.result({
+  "duration": 746067600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.validate_user_on_Time_Menu()"
+});
+formatter.result({
+  "duration": 26283000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Orange_StepDefination.close_the_Browser()"
+});
+formatter.result({
+  "duration": 1405568100,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 58100,
+  "status": "passed"
+});
+});
